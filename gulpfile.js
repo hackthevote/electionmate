d1e34eef09f36be733faf443a5c7611f433c4dc2
Szymon Wartak <szymon.wartak@gmail.com>
2016-04-23 16:06:04 +0100

    sass temp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
+    csso = require('gulp-csso'),
+    gutil = require('gulp-util');
+
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 9',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
+gulp.task('sass', function () {
+  return gulp.src('app/style/style.scss')
+      .pipe(sass())
+      .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
+      .pipe(csso())
+      .on('error', gutil.log)
+      .pipe(gulp.dest('stylesheets'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('app/style/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
