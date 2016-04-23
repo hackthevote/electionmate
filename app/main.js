3ee7ba55dfc15a233b89eeed913e02120ac04697
Matthew Wilkes <git@matthewwilkes.name>
2016-04-23 16:39:35 +0100

    Initial work towards cloud messaging

---
+++ app/main.js
@@ -0,0 +1,21 @@
+if ('serviceWorker' in navigator) {
+ console.log('Service Worker is supported');
+ navigator.serviceWorker.register('sw.js').then(function(reg) {
+   console.log(':^)', reg);
+   if ('serviceWorker' in navigator) {
+       console.log('Service Worker is supported');
+       navigator.serviceWorker.register('sw.js').then(function(reg) {
+           console.log(':^)', reg);
+           reg.pushManager.subscribe({
+               userVisibleOnly: true
+           }).then(function(sub) {
+               console.log('endpoint:', sub.endpoint);
+           });
+       }).catch(function(error) {
+           console.log(':^(', error);
+       });
+   }    
+ }).catch(function(err) {
+   console.log(':^(', err);
+ });
+}
