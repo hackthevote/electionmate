729aafc914102ee04145422a9c18568e7ded5682
Matthew Wilkes <git@matthewwilkes.name>
2016-05-23 21:40:36 +0100

    Updates

--- app/sw.js
+++ app/sw.js
@@ -7,6 +7,34 @@ self.addEventListener('activate', function(event) {
   console.log('Activated', event);
 });
 self.addEventListener('push', function(event) {
-  console.log('Push message received', event);
-  // TODO
+  console.log('Push message', event);
+  var title = 'Push message';
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: 'A voter registration deadline is approaching',
+      icon: 'img/squarelogo.png',
+      tag: 'deadline'
+    }));
+});
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('Notification click: tag ', event.notification.tag);
+    event.notification.close();
+    var url = 'https://electionmate.hackeurope.uk/';
+    event.waitUntil(
+        clients.matchAll({
+            type: 'window'
+        })
+        .then(function(windowClients) {
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
 });
