afe89ea8b40c8232b8fac69e5ad8190e3eff587b
Matthew Wilkes <git@matthewwilkes.name>
2016-06-08 17:14:25 +0100

    Send endpoint to cloud

--- app/main.js
+++ app/main.js
@@ -10,11 +10,25 @@ if ('serviceWorker' in navigator) {
                userVisibleOnly: true
            }).then(function(sub) {
                console.log('endpoint:', sub.endpoint);
+               var WebServiceUrl = "https://lyzmi1nt8f.execute-api.eu-west-1.amazonaws.com/prod?endpoint=" + sub.endpoint;
+               var DataToSend = {};
+               $.ajax({
+                   type: "PUT",
+                   contentType: "application/json; charset=utf-8",
+                   url: WebServiceUrl,
+                   data: JSON.stringify({}),
+                   dataType: "json",
+                   success: function (msg) {
+                   },
+                   error: function (err){
+                   }
+               });
+               
            });
        }).catch(function(error) {
            console.log(':^(', error);
        });
-   }    
+       }    
  }).catch(function(err) {
    console.log(':^(', err);
  });
